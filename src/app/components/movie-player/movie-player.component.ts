0c11fdea8a6792f42503ce87d143bb0886549d66
kintys <dima.sytnik92@gmail.com>
2024-09-25 22:39:15 +0200

    movie-player add new methods

--- src/app/components/movie-player/movie-player.component.ts
+++ src/app/components/movie-player/movie-player.component.ts
@@ -2,7 +2,7 @@ import { AfterContentInit, Component, ElementRef, Input, OnInit, ViewChild } fro
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 import { YoutubePlayerComponent } from 'ngx-youtube-player'
 import { SliderModule } from 'primeng/slider'
-import { BehaviorSubject, Subscription, interval } from 'rxjs'
+import { BehaviorSubject, Subject, Subscription, interval, takeUntil } from 'rxjs'
 @Component({
     selector: 'app-movie-player',
     standalone: true,
@@ -16,7 +16,12 @@ export class MoviePlayerComponent implements OnInit, AfterContentInit {
     @Input() id!: string
     player!: YT.Player
     currentVideoTime = new BehaviorSubject<string>('0')
+    statusVideo = new BehaviorSubject<number>(0)
     videoUpdateSubscription!: Subscription
+    bufferCurrentTime$ = new BehaviorSubject<string>('0')
+    bufferUpdateSubscription$!: Subscription
+    stopBufferInterval$ = new Subject<void>()
+    stopInterval$ = new Subject<void>()
     constructor(private sanitizer: DomSanitizer) {}
     ngOnInit(): void {
         this.safeUrl = this.getSafeUrl(this.id!)
@@ -26,34 +31,76 @@ export class MoviePlayerComponent implements OnInit, AfterContentInit {
         const url = `https://www.youtube.com/embed/${key}`
         return this.sanitizer.bypassSecurityTrustResourceUrl(url)
     }
-    ngAfterContentInit(): void {}
-
+    ngAfterContentInit(): void {
+        this.statusVideo.subscribe((status) => {
+            switch (status) {
+                case 1:
+                    this.playVideo()
+                    break
+                case 2:
+                    this.stopVideo()
+                    break
+                case 3:
+                    this.bufferTimeline()
+                    break
+                case 0:
+                    this.endVideo()
+                    break
+                default:
+                    break
+            }
+        })
+    }
     getCurrentVideoTime() {
-        return `${this.player.getCurrentTime() / this.player.getDuration()}`
+        return (this.player.getCurrentTime() / this.player.getDuration()).toString()
     }
     savePlayer(player: any) {
         this.player = player
-        console.log('player instance', player)
     }
-    // onStateChange(event: any) {
-    //     console.log('player state', event.data)
-    // }
+    onStateChange(event: any) {
+        this.statusVideo.next(event.data)
+    }
     showDuring($event: any) {
-        console.log($event)
         const rect = this.timelineContainer?.nativeElement.getBoundingClientRect()
-        console.log(rect)
         const time = Math.min(Math.max(0, $event.x - rect.x), rect.width) / rect.width
+        this.currentVideoTime.next(this.getCurrentVideoTime())
         this.player.seekTo(time * this.player.getDuration(), true)
-    }
-    showPlay() {
-        this.player.playVideo()
+        // this.showVideoTimeLine()
+
         this.currentVideoTime.subscribe((val) => {
-            document.body.style.setProperty('--progress-position', val)
-            console.log(val)
+            this.timelineContainer?.nativeElement.style.setProperty('--progress-position', val)
         })
-        this.videoUpdateSubscription = interval(100).subscribe(() => {
-            const videoTime = this.getCurrentVideoTime()
-            this.currentVideoTime.next(videoTime)
+    }
+    playVideo() {
+        this.showVideoTimeLine()
+        this.player.playVideo()
+    }
+    showVideoTimeLine() {
+        this.videoUpdateSubscription = interval(100)
+            .pipe(takeUntil(this.stopInterval$))
+            .subscribe(() => {
+                console.log(2123)
+                this.currentVideoTime.next(this.getCurrentVideoTime())
+            })
+        this.currentVideoTime.subscribe((val) => {
+            this.timelineContainer?.nativeElement.style.setProperty('--progress-position', val)
         })
+    }
+    stopVideo() {
+        this.player.pauseVideo()
+        this.stopInterval$.next()
+    }
+    bufferTimeline() {
+        this.bufferUpdateSubscription$ = interval(1000)
+            .pipe(takeUntil(this.stopBufferInterval$))
+            .subscribe(() => this.bufferCurrentTime$.next(this.player.getVideoLoadedFraction().toString()))
+        this.bufferCurrentTime$.pipe(takeUntil(this.stopBufferInterval$)).subscribe((timeUpdate) => {
+            if (timeUpdate === '1') this.stopBufferInterval$.next()
+            this.timelineContainer?.nativeElement.style.setProperty('--buffering-progress', timeUpdate)
+        })
+    }
+    endVideo() {
+        this.stopInterval$.next()
+        this.currentVideoTime.next('1')
     }
 }
